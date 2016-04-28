08dae1f7eccbf24d791aac550a05c2c9579a99cd
Tim Stoop <tim@kumina.nl>
2016-04-28 14:52:00 +0200

    This is just a test app that I used to check variables.

---
+++ apache-app/redirect.py
@@ -0,0 +1,15 @@
+from mod_python import apache
+import sqlite3
+import re
+
+
+def handler(req):
+    conn = sqlite3.connect('redirects.sql')
+    req.content_type = 'text/plain'
+    req.write("The request: %s\n" % req.the_request)
+    req.write("Host: %s\n" % req.hostname)
+    req.write("URI: %s\n" % req.uri)
+    req.write("Filename: %s\n" % req.filename)
+    req.write("Unparsed URI: %s\n" % req.unparsed_uri)
+    return apache.OK
+
